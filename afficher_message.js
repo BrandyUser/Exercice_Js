2edced78ea024d996a7d49700ac0fb3bc41354f4
robert mafuta <125672896+BrandyUser@users.noreply.github.com>
2024-11-29 19:18:31 +0100

    afficher nombreEntier ex 1-10

--- afficher_message.js
+++ afficher_message.js
@@ -1,1 +1,11 @@
-document.getElementById("resultat").innerHTML = "Bonjour Cher Client";
+//*Exercie 1: Afficher le message*//
+//*document.getElementById("resultat").innerHTML = "Bonjour Cher Client";*//
+
+let nombreEntier= '';
+for (let i = 1; i <= 10; i++) {
+if ( i % 2 === 0) {
+    nombreEntier += i + '';
+}
+}
+console.log(nombreEntier);
+
